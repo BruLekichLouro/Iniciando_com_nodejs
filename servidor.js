30764814021c0617efe932b6a9ef3aab53b17857
BruLekichLouro <bububio@yahoo.com.br>
2022-08-20 14:08:35 -0300

    Adicionando comando para que envia resposta e encerra servidor

--- servidor.js
+++ servidor.js
@@ -1,2 +1,3 @@
 const http = require('http');
-http.createServer((req, res) => {res.write('<h1>Working<h1>')}).listen(3000, ()=> console.log("Servidor rodando"));
+http.createServer((req, res) => 
+{res.end('<h1>Working<h1>')}).listen(3000, ()=> console.log("Servidor rodando"));
